f4f3e3fe7a97460a5bca33d533d165984c751513
Kira Lu <newclearex@gmail.com>
2023-09-18 16:56:17 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import { create } from 'random-seed';
+
+type Options = {
+    bias: 'left' | 'right' | 'random';
+    seed: string | null;
+    skip: number;
+    cellTypes: {
+        id: number;
+        behavior: 'empty' | 'static' | 'dynamic';
+    }[]
+}
+
+const defaultOptions: Options = {
+    bias: 'random',
+    seed: null,
+    skip: 0,
+    cellTypes: [
+        { id: 0, behavior: 'empty' },
+        { id: 1, behavior: 'static' },
+        { id: 2, behavior: 'dynamic' }
+    ],
+};
+
+export function step(data: number[][], options?: Partial<Options>) {
+    const finalOptions: Options = { ...defaultOptions, ...options };
+
+    // Make sure data is a 2D array
+    if (!data[0] || data[0].length === 0) {
+        throw new Error('Invalid data');
+    }
+    // Make sure data is rectangular
+    const width = data[0].length;
+    if (data.some(row => row.length !== width)) {
+        throw new Error('Invalid data');
+    }
+
+    const newData = data.map(row => [...row]);
+
+    const rand = finalOptions.seed ? create(finalOptions.seed) : create();
+
+    const behaviors = new Map<number, 'empty' | 'static' | 'dynamic'>();
+    for (const type of finalOptions.cellTypes) {
+        behaviors.set(type.id, type.behavior);
+    }
+
+    // Loop through for each skip
+    for (let i = 0; i < finalOptions.skip + 1; i++) {
+        // Loop through each cell
+        for (const [y, row] of newData.entries()) {
+            for (const [x, cell] of row.entries()) {
+                const behavior = behaviors.get(cell);
+                if (!behavior || behaviors.get(cell) === 'empty' || behaviors.get(cell) === 'static') {
+                    continue;
+                }
+
+                // If empty underneath, fall
+                const belowRow = newData[y + 1];
+                if (!belowRow) return;
+
+                const belowCell = belowRow && belowRow[x];
+                const belowCellBehavior = belowCell === undefined ? undefined : behaviors.get(belowCell);
+                if (belowCellBehavior === 'empty') {
+                    row[x] = 0;
+                    belowRow[x] = cell;
+                    continue;
+                }
+
+                // If empty to the left or right, move
+                const bias =
+                    finalOptions.bias === 'left' ?
+                        0 :
+                        finalOptions.bias === 'right' ?
+                            1 :
+                            rand(2);
+
+                const offset = bias === 0 ? -1 : 1;
+
+                {
+                    const targetCell = belowRow[x + offset];
+                    const targetCellBehavior = targetCell === undefined ? undefined : behaviors.get(targetCell);
+                    if (targetCellBehavior === 'empty') {
+                        row[x] = 0;
+                        belowRow[x + offset] = cell;
+                        continue;
+                    }
+                }
+                {
+                    const targetCell = belowRow[x - offset];
+                    const targetCellBehavior = targetCell === undefined ? undefined : behaviors.get(targetCell);
+                    if (targetCellBehavior === 'empty') {
+                        row[x] = 0;
+                        belowRow[x - offset] = cell;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    return newData;
+}
